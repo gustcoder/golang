4fe8165fc5bd6ab76094b5d619ea6317e23fd26c
gustcoder <gustavofaramos@gmail.com>
2021-09-09 21:55:40 -0300

    Secao 2 - Pacotes Externos

--- 1 - Pacotes/auxiliar/auxiliar2.go
+++ 1 - Pacotes/auxiliar/auxiliar2.go
@@ -1,7 +1,14 @@
 package auxiliar
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/badoux/checkmail"
+)
 
 func escrever2() {
 	fmt.Println("Escrevendo pela funcao escrever2")
+
+	erro := checkmail.ValidateFormat("teste@email.com")
+	fmt.Println(erro)
 }
