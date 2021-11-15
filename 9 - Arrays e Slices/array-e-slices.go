6386b8162ef9e8c23facc713310dc780f4c7efa3
gustcoder <gustavofaramos@gmail.com>
2021-11-15 10:56:47 -0300

    Arrays Internos

--- 9 - Arrays e Slices/array-e-slices.go
+++ 9 - Arrays e Slices/array-e-slices.go
@@ -5,9 +5,11 @@ import "fmt"
 func main() {
 	var array1 [5]string
 
+	// declaracao padrao
 	array1[0] = "Posição 1"
 	fmt.Println(array1)
 
+	// declaracao por inferencia de  tipo
 	array2 := [2]string{"inferencia", "tipo"}
 	fmt.Println(array2)
 
@@ -24,4 +26,25 @@ func main() {
 
 	array3[2] = 99 // por referencia, altera a fatia do array
 	fmt.Println(slice2)
+
+	// Arrays Internos (tipo, tamanho, capacidade maxima, que é opcional)
+	slice3 := make([]float32, 10, 11)
+	fmt.Println(slice3)
+
+	slice3 = append(slice3, 5)
+	// se tentar extrapolar a capcidade, o Go
+	// se encarrega de "dobrar" a capacidade do array
+	// interno, fazendo o slice ter capacidade ilimitada
+	slice3 = append(slice3, 6)
+
+	fmt.Println(slice3)
+	fmt.Println(len(slice3))
+	fmt.Println(cap(slice3))
+
+	slice4 := make([]int)
+	fmt.Println(slice4)
+
+	slice4 = append(slice4, 10)
+	fmt.Println(len(slice4))
+	fmt.Println(cap(slice4))
 }
