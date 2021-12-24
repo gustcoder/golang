66761d1e2fe48f4b964e29fda5810aca7893908c
gustcoder <gustavofaramos@gmail.com>
2021-12-24 11:07:52 -0300

    Finalizando app Linha de Comando

--- 17 - Linha de Comando/app/app.go
+++ 17 - Linha de Comando/app/app.go
@@ -1,11 +1,66 @@
 package app
 
-import "github.com/urfave/cli"
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/urfave/cli"
+)
 
 func Gerar() *cli.App {
 	app := cli.NewApp()
 	app.Name = "App Linha de Comando"
 	app.Usage = "Busca IPs e nomes de servidores"
 
+	flags := []cli.Flag{
+		cli.StringFlag{
+			Name:  "host",
+			Value: "devbook.com.br",
+		},
+	}
+
+	app.Commands = []cli.Command{
+		{
+			Name:   "get-ip",
+			Usage:  "Busca IPs de endereços na internet",
+			Flags:  flags,
+			Action: getIps,
+		},
+		{
+			Name:   "get-server",
+			Usage:  "Busca Servidores na internet",
+			Flags:  flags,
+			Action: getServers,
+		},
+	}
+
 	return app
+}
+
+func getIps(c *cli.Context) {
+	host := c.String("host")
+
+	ips, erro := net.LookupIP(host)
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	for _, ip := range ips {
+		fmt.Println(ip)
+	}
+}
+
+func getServers(c *cli.Context) {
+	host := c.String("host")
+
+	servers, erro := net.LookupNS(host)
+
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	for _, server := range servers {
+		fmt.Println(server.Host)
+	}
 }
