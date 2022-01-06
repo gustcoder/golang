bcada40c498e77b04590dfc688699cc8684c30bc
gustcoder <gustavofaramos@gmail.com>
2022-01-05 22:33:03 -0300

    Canais

---
+++ 18 - Concorrência/canais/canais.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	canal := make(chan string)
+
+	go escrever("Olá mundo", canal)
+
+	for {
+		mensagem, canalAberto := <-canal
+		if !canalAberto {
+			break
+		}
+
+		fmt.Println(mensagem)
+	}
+}
+
+func escrever(texto string, canal chan string) {
+	for i := 0; i < 5; i++ {
+		canal <- texto
+		time.Sleep(time.Second)
+	}
+
+	close(canal)
+}
