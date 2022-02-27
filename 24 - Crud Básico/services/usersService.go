879569cc328582b5552766995b703b5a689e960d
gustcoder <gustavofaramos@gmail.com>
2022-02-27 12:02:39 -0300

    Implementando retornos na msg de erro

--- 24 - Crud Básico/services/usersService.go
+++ 24 - Crud Básico/services/usersService.go
@@ -19,7 +19,11 @@ type User struct {
 	Email string `json:"email"`
 }
 
-func ResponseError(rw http.ResponseWriter, msg string) {
+func ResponseError(rw http.ResponseWriter, msg string, statusCode int) {
+	if statusCode > 0 {
+		rw.WriteHeader(statusCode)
+	}
+
 	rw.Write([]byte(msg))
 	return
 }
@@ -27,7 +31,7 @@ func ResponseError(rw http.ResponseWriter, msg string) {
 // interface{} => como se fosse o mixed, onde pode chegar qualquer tipo
 func ResponseOK(rw http.ResponseWriter, msg string, parameters interface{}) {
 	if err := json.NewEncoder(rw).Encode(parameters); err != nil {
-		ResponseError(rw, msg)
+		ResponseError(rw, msg, http.StatusInternalServerError)
 	}
 }
 
@@ -45,40 +49,44 @@ func GetUser(rw http.ResponseWriter, r *http.Request) {
 
 	ID, err := strconv.ParseUint(request["id"], 10, 32)
 	if err != nil {
-		ResponseError(rw, "Error converting ID to int")
+		ResponseError(rw, "Error converting ID to int", http.StatusInternalServerError)
 	}
 
 	db, err := db.Connect()
 	if err != nil {
-		ResponseError(rw, "Error connecting database")
+		ResponseError(rw, "Error connecting database", http.StatusInternalServerError)
 	}
 	defer db.Close()
 
 	userInfo, err := db.Query("SELECT * FROM usuarios WHERE id = ?", ID)
 	if err != nil {
-		ResponseError(rw, "Error on getting user ID")
+		ResponseError(rw, "Error on getting user ID", http.StatusInternalServerError)
 	}
 
 	var user User
 	if userInfo.Next() {
 		if err := userInfo.Scan(&user.ID, &user.Nome, &user.Email); err != nil {
-			ResponseError(rw, "Error scanning userInfo")
+			ResponseError(rw, "Error scanning userInfo", http.StatusInternalServerError)
 		}
 	}
 
-	ResponseOK(rw, "Error on show userInfo", user)
+	if user.ID == 0 {
+		ResponseError(rw, "User not found", http.StatusNotFound)
+	} else {
+		ResponseOK(rw, "Error on show userInfo", user)
+	}
 }
 
 func GetUsers(rw http.ResponseWriter, r *http.Request) {
 	db, err := db.Connect()
 	if err != nil {
-		ResponseError(rw, "Error connecting database")
+		ResponseError(rw, "Error connecting database", http.StatusInternalServerError)
 	}
 	defer db.Close()
 
 	usersList, err := db.Query("SELECT * FROM usuarios")
 	if err != nil {
-		ResponseError(rw, "Error on Select")
+		ResponseError(rw, "Error on Select", http.StatusInternalServerError)
 	}
 
 	// criando um slice para armazenar objetos dos usuarios
@@ -110,36 +118,36 @@ func NewUser(rw http.ResponseWriter, r *http.Request) {
 	request, err := ioutil.ReadAll(r.Body)
 
 	if err != nil {
-		ResponseError(rw, "Error reading NewUser request body")
+		ResponseError(rw, "Error reading NewUser request body", http.StatusInternalServerError)
 	}
 
 	var user User
 
 	if err := json.Unmarshal(request, &user); err != nil {
-		ResponseError(rw, "Error converting User to struct")
+		ResponseError(rw, "Error converting User to struct", http.StatusInternalServerError)
 	}
 
 	db, err := db.Connect()
 	if err != nil {
-		ResponseError(rw, "Error connecting database")
+		ResponseError(rw, "Error connecting database", http.StatusInternalServerError)
 	}
 	defer db.Close()
 
 	statement, err := db.Prepare("INSERT INTO usuarios (nome, email) VALUES (?, ?)")
 
 	if err != nil {
-		ResponseError(rw, "Error on Prepare statement")
+		ResponseError(rw, "Error on Prepare statement", http.StatusInternalServerError)
 	}
 	defer db.Close()
 
 	insert, err := statement.Exec(user.Nome, user.Email)
 	if err != nil {
-		ResponseError(rw, "Error on Insert")
+		ResponseError(rw, "Error on Insert", http.StatusInternalServerError)
 	}
 
 	lastUserId, err := insert.LastInsertId()
 	if err != nil {
-		ResponseError(rw, "Error getting Last Inserted User ID")
+		ResponseError(rw, "Error getting Last Inserted User ID", http.StatusInternalServerError)
 	}
 
 	fmt.Println(user) // including response to server
